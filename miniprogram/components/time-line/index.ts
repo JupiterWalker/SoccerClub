fd109bb78e542724fae0f92b8c74c5c27a62a3d1
cwang14 <cwang14@ebay.com>
2024-11-28 12:24:28 +0800

    API call: - create activity  - signUp

--- miniprogram/components/time-line/index.ts
+++ miniprogram/components/time-line/index.ts
@@ -4,21 +4,37 @@ var app = getApp<IAppOption>()
 Component({
   data:{
     dataList:[
-      {'date':'2023-05-27','part':'18:00-20:00','content':'地点:初晟足球场\n参加人数: 24'},
-      {'date':'2023-05-28','part':'18:00-20:00','content':'地点:初晟足球场\n参加人数: 24'},
-      {'date':'2023-05-29','part':'18:00-20:00','content':'地点:初晟足球场\n参加人数: 24'},
-      {'date':'2023-05-30','part':'18:00-20:00','content':'地点:初晟足球场\n参加人数: 24'},
-      {'date':'2023-05-31','part':'18:00-20:00','content':'地点:初晟足球场\n参加人数: 24'},
+      {'datetime':'2023-05-27 18:00-20:00','location':'初晟足球场', 'latitude': 1.111, 'longitude': 1.1111, 'status': '火热报名中', 'color': 'red', 'activity_id': 1, 'type': '球赛⚽', 'headcount': 25, 'comment': ''},
+      {'datetime':'2023-05-27 18:00-20:00','location':'初晟足球场', 'latitude': 1.111, 'longitude': 1.1111, 'status': '已结束', 'color': 'green', 'activity_id': 2, 'type': '球赛⚽', 'headcount': 25, 'comment': ''},
+      {'datetime':'2023-05-27 18:00-20:00','location':'初晟足球场', 'latitude': 1.111, 'longitude': 1.1111, 'status': '已取消', 'color': 'gray', 'activity_id': 3, 'type': '球赛⚽', 'headcount': 25, 'comment': ''},
+      {'datetime':'2023-05-27 18:00-20:00','location':'东北地锅鸡', 'latitude': 1.111, 'longitude': 1.1111, 'status': '已结束', 'color': 'green', 'activity_id': 4, 'type': '聚餐🍻', 'headcount': 25, 'comment': ''},
+      {'datetime':'2023-05-27 18:00-20:00','location':'初晟足球场', 'latitude': 1.111, 'longitude': 1.1111, 'status': '已结束', 'color': 'green', 'activity_id': 5, 'type': '球赛⚽', 'headcount': 25, 'comment': ''},
     ],
   },
   methods: {
-    onCheckDetail() {
-    console.log("time-line onCheckDetail")
-    wx.navigateTo({
-      url: '/pages/historyActivity/index'
-    });
-  }
+    onCheckDetail(e) {
+    console.log("time-line onCheckDetail e: ", e)
+    for (let i of this.data.dataList) {
+      if (i.activity_id == e.currentTarget.dataset.id) {
+        let status = i.status;
+        let type = i.type;
+        let datetime = i.datetime;
+        let headcount = i.headcount;
+        let comment = i.comment;
+        let location = i.location
+        let latitude = i.latitude
+        let longitude = i.longitude
+        // 构建包含所有参数的 URL
+        const url = `/pages/historyActivity/index?activity_id=${i.activity_id}&status=${encodeURIComponent(status)}&type=${encodeURIComponent(type)}&datetime=${encodeURIComponent(datetime)}&headcount=${headcount}&comment=${encodeURIComponent(comment)}&location=${encodeURIComponent(location)}&latitude=${encodeURIComponent(latitude)}&longitude=${encodeURIComponent(longitude)}`;
+
+        // 使用 wx.navigateTo 跳转到详情页面，并传递所有参数
+        wx.navigateTo({
+          url: url
+        });
+      }
+    }
   },
+  }
 })
 
 
--- miniprogram/pages/historyActivity/index.ts
+++ miniprogram/pages/historyActivity/index.ts
@@ -1,56 +1,38 @@
 var app = getApp()
 
-Page({
+Component({
   data: {
-    text: "This is page data."
+    datetime: "null",
+    headCount: 0,
+    comment: "no comment",
+    longitude: 121.379577,
+    latitude: 31.114569,
+    locationText: "chusheng",
+    memberInfo: [
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jim"},
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jim"},
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jimmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"},
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jim"},
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jim"},
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jim"},
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jim"},
+      {"avatarUrl": "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg", "nickName": "Jim"}]
   },
-  onLoad: function() {
-    // 页面创建时执行
-    console.log("signUp onload")
-  },
-  onShow: function() {
-    // 页面出现在前台时执行
-  },
-  onReady: function() {
-    // 页面首次渲染完毕时执行
-  },
-  onHide: function() {
-    // 页面从前台变为后台时执行
-  },
-  onUnload: function() {
-    // 页面销毁时执行
-  },
-  onPullDownRefresh: function() {
-    // 触发下拉刷新时执行
-  },
-  onReachBottom: function() {
-    // 页面触底时执行
-  },
-  onShareAppMessage: function () {
-    // 页面被用户分享时执行
-  },
-  onPageScroll: function() {
-    // 页面滚动时执行
-  },
-  onResize: function() {
-    // 页面尺寸变化时执行
-  },
-  onTabItemTap(item) {
-    // tab 点击时执行
-    console.log(item.index)
-    console.log(item.pagePath)
-    console.log(item.text)
-  },
-  // 事件响应函数
-  viewTap: function() {
-    this.setData({
-      text: 'Set some data for updating view.'
-    }, function() {
-      // this is setData callback
-    })
-  },
-  // 自由数据
-  customData: {
-    hi: 'MINA'
+  methods: {
+    onLoad(options) {
+      // 
+      console.log("historyActivity Received parameters:", options);
+      this.setData({
+        activityId: options.activity_id,
+        status: decodeURIComponent(options.status),
+        type: decodeURIComponent(options.type),
+        datetime: decodeURIComponent(options.datetime),
+        headCount: Number(options.headcount),
+        comment: decodeURIComponent(options.comment),
+        location: decodeURIComponent(options.location),
+        latitude: Number(decodeURIComponent(options.latitude)),
+        longitude: Number(decodeURIComponent(options.longitude)),
+        });
+      },
   }
 })
